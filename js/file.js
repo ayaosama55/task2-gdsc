81cc1a559476a448684543fddb4181ea4ced5344
Aya Osama <aya55osma2001@gmail.com>
2021-11-12 13:34:45 +0200

    added brand section

--- js/file.js
+++ js/file.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    
+    toTopButton = $(".totop");
+    $(window).on("scroll", function(){
+        if($(window).scrollTop() > 700){
+            toTopButton.fadeIn();
+        } else { 
+            toTopButton.fadeOut();
+        }
+    });
+    toTopButton.on("click", (e) => {
+        e.preventDefault();
+        document.querySelector(".header").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+    $(".down").on("click", (e) => {
+        e.preventDefault();
+        document.querySelector(".about-us").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
 });
